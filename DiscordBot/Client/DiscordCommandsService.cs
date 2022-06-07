2968f337401b31b7cf8f84187c9b964fa9ff92b0
TehGM <TehGM@users.noreply.github.com>
2022-06-07 09:46:02 +0100

    Added commands service

---
+++ DiscordBot/Client/DiscordCommandsService.cs
@@ -0,0 +1,63 @@
+﻿using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Hosting;
+
+namespace DevSubmarine.DiscordBot.Client
+{
+    internal class DiscordCommandsService : IHostedService, IDisposable
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly DiscordOptions _options;
+        private readonly InteractionService _interactions;
+        private readonly IServiceProvider _services;
+        private readonly ILogger _log;
+
+        public DiscordCommandsService(DiscordSocketClient client, IServiceProvider services, ILogger<DiscordCommandsService> log, IOptions<DiscordOptions> options)
+        {
+            this._client = client;
+            this._services = services;
+            this._log = log;
+            this._options = options.Value;
+            this._interactions = new InteractionService(this._client, new InteractionServiceConfig()
+            {
+                DefaultRunMode = RunMode.Async,
+                UseCompiledLambda = this._options.CompileCommands
+            });
+
+            this._client.Ready += OnClientReady;
+            this._interactions.Log += OnLog;
+        }
+
+        private async Task OnClientReady()
+        {
+            await this._interactions.AddModulesAsync(this.GetType().Assembly, this._services);
+
+            if (this._options.CommandsGuildID != null)
+                await this._interactions.RegisterCommandsToGuildAsync(this._options.CommandsGuildID.Value).ConfigureAwait(false);
+            else
+                await this._interactions.RegisterCommandsGloballyAsync().ConfigureAwait(false);
+        }
+
+        private Task OnLog(Discord.LogMessage logMessage)
+        {
+            this._log.Log(logMessage);
+            return Task.CompletedTask;
+        }
+
+        Task IHostedService.StartAsync(CancellationToken cancellationToken)
+            => Task.CompletedTask;
+
+        Task IHostedService.StopAsync(CancellationToken cancellationToken)
+        {
+            this.Dispose();
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            try { this._client.Ready -= OnClientReady; } catch { }
+            try { this._interactions.Log += OnLog; } catch { }
+            try { this._interactions?.Dispose(); } catch { }
+        }
+    }
+}
--- DiscordBot/Client/DiscordOptions.cs
+++ DiscordBot/Client/DiscordOptions.cs
@@ -2,6 +2,11 @@
 {
     public class DiscordOptions
     {
+        /// <summary>Bot token from Discord API portal.</summary>
         public string BotToken { get; set; }
+        /// <summary>ID of guild to register commands to. Should be null, unless testing.</summary>
+        public ulong? CommandsGuildID { get; set; }
+        /// <summary>Compiles commands, which improves their execution speed, but increases memory use.</summary>
+        public bool CompileCommands { get; set; }
     }
 }
