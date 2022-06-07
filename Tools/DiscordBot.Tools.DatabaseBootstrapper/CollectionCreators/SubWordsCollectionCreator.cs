4e45195c901ad41d0ddb1a52db1dd9c518d73f00
TehGM <TehGM@users.noreply.github.com>
2022-06-07 11:17:51 +0100

    Added DB bootstrapper tool

---
+++ Tools/DiscordBot.Tools.DatabaseBootstrapper/CollectionCreators/SubWordsCollectionCreator.cs
@@ -0,0 +1,34 @@
+﻿using DevSubmarine.DiscordBot.SubWords;
+
+namespace DevSubmarine.DiscordBot.Tools.DatabaseBootstrapper.CollectionCreators
+{
+    class SubWordsCollectionCreator : CollectionCreatorBase<SubWord>
+    {
+        protected override string CollectionName { get; }
+
+        public SubWordsCollectionCreator(IMongoDatabaseClient client, ILogger<SubWordsCollectionCreator> log, IOptions<MongoOptions> options)
+            : base(client, log, options)
+        {
+            this.CollectionName = options.Value.SubWordsCollectionName;
+        }
+
+        public override async Task ProcessCollectionAsync(CancellationToken cancellationToken = default)
+        {
+            IMongoCollection<SubWord> collection = await base.GetOrCreateCollectionAsync(cancellationToken).ConfigureAwait(false);
+
+            await this.CreateAuthorIdIndex(collection, cancellationToken).ConfigureAwait(false);
+        }
+
+        private Task CreateAuthorIdIndex(IMongoCollection<SubWord> collection, CancellationToken cancellationToken = default)
+        {
+            base.Log.LogDebug("Creating index on property {Property} in {Collection}", nameof(SubWord.AuthorID), CollectionName);
+            return collection.Indexes.CreateOneAsync(new CreateIndexModel<SubWord>(
+                Builders<SubWord>.IndexKeys.Ascending(k => k.AuthorID),
+                new CreateIndexOptions<SubWord>()
+                {
+                    Unique = false
+                }),
+                null, cancellationToken);
+        }
+    }
+}
