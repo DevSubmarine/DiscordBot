3462a3733f078ea107bbb55116f0ec72c4eef0ab
TehGM <TehGM@users.noreply.github.com>
2022-06-17 19:14:45 +0100

    Tests for blog channel activator

--- Tests/DiscordBot.Tests/TestBase.cs
+++ Tests/DiscordBot.Tests/TestBase.cs
@@ -5,6 +5,10 @@ global using FluentAssertions;
 global using NSubstitute;
 
 global using System;
+global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using System.Collections.Generic;
 global using Microsoft.Extensions.Options;
 
 namespace DevSubmarine.DiscordBot.Tests
