9ccaf9264855193418abd214ee43f19586aecbf5
TehGM <TehGM@users.noreply.github.com>
2022-07-16 22:04:02 +0100

    Added random dev sub member status placeholder

---
+++ DiscordBot/Features/RandomStatus/Placeholders/RandomDevSubMemberName.cs
@@ -0,0 +1,32 @@
+﻿using Discord;
+using TehGM.Utilities.Randomization;
+
+namespace DevSubmarine.DiscordBot.RandomStatus.Placeholders
+{
+    [StatusPlaceholder("{{RandomDevSubMemberName}}")]
+    internal class RandomDevSubMemberName : IStatusPlaceholder
+    {
+        private readonly IDiscordClient _client;
+        private readonly IRandomizer _randomizer;
+        private readonly DevSubOptions _options;
+
+        public RandomDevSubMemberName(IDiscordClient client, IRandomizer randomizer, IOptionsSnapshot<DevSubOptions> options)
+        {
+            this._client = client;
+            this._randomizer = randomizer;
+            this._options = options.Value;
+        }
+
+        public async Task<string> GetReplacementAsync(Match placeholder, CancellationToken cancellationToken = default)
+        {
+            IGuild guild = await this._client.GetGuildAsync(this._options.GuildID, CacheMode.AllowDownload, cancellationToken.ToRequestOptions()).ConfigureAwait(false);
+            IEnumerable<IGuildUser> members = await guild.GetUsersAsync(CacheMode.AllowDownload, cancellationToken.ToRequestOptions()).ConfigureAwait(false);
+
+            if (!members.Any())
+                throw new InvalidOperationException($"Guild {this._options.GuildID} has no members to pick from");
+
+            IGuildUser randomMember = this._randomizer.GetRandomValue(members);
+            return randomMember.Nickname ?? randomMember.Username;
+        }
+    }
+}
---
+++ Tests/DiscordBot.Tests/Features/RandomStatus/Placeholders/RandomDevSubMemberNameTests.cs
@@ -0,0 +1,87 @@
+﻿using DevSubmarine.DiscordBot.RandomStatus.Placeholders;
+using TehGM.Utilities.Randomization;
+using TehGM.Utilities.Randomization.Services;
+
+namespace DevSubmarine.DiscordBot.Tests.Features.RandomStatus.Placeholders
+{
+    [TestOf(typeof(RandomDevSubMemberName))]
+    public class RandomDevSubMemberNameTests : PlaceholderTestBase
+    {
+        protected override Type PlaceholderType => typeof(RandomDevSubMemberName);
+
+        public override void SetUp()
+        {
+            base.SetUp();
+
+            base.Fixture.Register<IRandomizer>(() => new RandomizerService());
+        }
+
+        [Test, AutoNSubstituteData]
+        [Repeat(3)]
+        [Category(nameof(ChannelName.GetReplacementAsync))]
+        public async Task GetReplacement_WithNicknames_ReturnsRandomUsername(IDictionary<string, string> users)
+        {
+            this.CreateGuild(users);
+            IEnumerable<string> validResults = users.Values;
+
+            RandomDevSubMemberName placeholder = base.Fixture.Create<RandomDevSubMemberName>();
+            string result = await placeholder.GetReplacementAsync(base.CreateDefaultTestMatch());
+
+            result.Should().BeOneOf(validResults);
+        }
+
+        [Test, AutoNSubstituteData]
+        [Repeat(3)]
+        [Category(nameof(ChannelName.GetReplacementAsync))]
+        public async Task GetReplacement_WithoutNicknames_ReturnsRandomUsername(IEnumerable<string> users)
+        {
+            this.CreateGuild(users.ToDictionary<string, string, string>(u => u, _ => null));
+            IEnumerable<string> validResults = users;
+
+            RandomDevSubMemberName placeholder = base.Fixture.Create<RandomDevSubMemberName>();
+            string result = await placeholder.GetReplacementAsync(base.CreateDefaultTestMatch());
+
+            result.Should().BeOneOf(validResults);
+        }
+
+        [Test]
+        [Category(nameof(ChannelName.GetReplacementAsync))]
+        public async Task GetReplacement_NoUsersFound_Throws()
+        {
+            this.CreateGuild(Enumerable.Empty<KeyValuePair<string, string>>());
+
+            RandomDevSubMemberName placeholder = base.Fixture.Create<RandomDevSubMemberName>();
+            Func<Task> act = async () => await placeholder.GetReplacementAsync(base.CreateDefaultTestMatch());
+
+            await act.Should().ThrowAsync<InvalidOperationException>();
+        }
+
+        [Test, AutoNSubstituteData]
+        [Category(nameof(ChannelName.GetReplacementAsync))]
+        public async Task GetReplacement_CalledMultipleTimes(IDictionary<string, string> users)
+        {
+            IGuild guild = this.CreateGuild(users);
+
+            RandomDevSubMemberName placeholder = base.Fixture.Create<RandomDevSubMemberName>();
+            await placeholder.GetReplacementAsync(base.CreateDefaultTestMatch());
+            await placeholder.GetReplacementAsync(base.CreateDefaultTestMatch());
+
+            await guild.Received(2).GetUsersAsync(Arg.Any<CacheMode>(), Arg.Any<RequestOptions>());
+        }
+
+        private IGuild CreateGuild(IEnumerable<KeyValuePair<string, string>> usernamesAndNicknames)
+        {
+            IGuild guild = Substitute.For<IGuild>();
+            guild.GetUsersAsync(Arg.Any<CacheMode>(), Arg.Any<RequestOptions>())
+                .Returns(x => usernamesAndNicknames.Select(u =>
+                {
+                    IGuildUser user = Substitute.For<IGuildUser>();
+                    user.Username.Returns(u.Key);
+                    user.Nickname.Returns(u.Value);
+                    return user;
+                }).ToArray());
+            base.Fixture.Freeze<IDiscordClient>().GetGuildAsync(Arg.Any<ulong>(), Arg.Any<CacheMode>(), Arg.Any<RequestOptions>()).Returns(guild);
+            return guild;
+        }
+    }
+}
