1f2c107202b850c23f3e2f09bd8370fa7406a389
TehGM <TehGM@users.noreply.github.com>
2022-07-29 19:17:17 +0100

    Add user birthdays store

---
+++ DiscordBot/Features/Birthdays/IUserBirthdaysStore.cs
@@ -0,0 +1,9 @@
+﻿namespace DevSubmarine.DiscordBot.Birthdays
+{
+    public interface IUserBirthdaysStore
+    {
+        Task<IEnumerable<UserBirthday>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<UserBirthday> GetAsync(ulong userID, CancellationToken cancellationToken = default);
+        Task UpdateAsync(UserBirthday birthday, CancellationToken cancellationToken = default);
+    }
+}
---
+++ DiscordBot/Features/Birthdays/UserBirthdaysDependencyInjectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using DevSubmarine.DiscordBot.Birthdays;
+using DevSubmarine.DiscordBot.Birthdays.Services;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class UserBirthdaysDependencyInjectionExtensions
+    {
+        public static IServiceCollection AddUserBirthdays(this IServiceCollection services)
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+
+            services.TryAddTransient<IUserBirthdaysStore, MongoUserBirthdayStore>();
+
+            return services;
+        }
+    }
+}
