c613613028152d3df947015344f104b9bf2a71cb
TehGM <TehGM@users.noreply.github.com>
2022-07-29 15:29:44 +0100

    Remove unused analyzer tests

--- Analyzers/DiscordInteractions/DiscordInteractions.Test/DiscordInteractionsUnitTests.cs
+++ Analyzers/DiscordInteractions/DiscordInteractions.Test/DiscordInteractionsUnitTests.cs
@@ -9,51 +9,5 @@ namespace DiscordInteractions.Test
     [TestClass]
     public class DiscordInteractionsUnitTest
     {
-        //No diagnostics expected to show up
-        [TestMethod]
-        public async Task TestMethod1()
-        {
-            var test = @"";
-
-            await VerifyCS.VerifyAnalyzerAsync(test);
-        }
-
-        //Diagnostic and CodeFix both triggered and checked for
-        [TestMethod]
-        public async Task TestMethod2()
-        {
-            var test = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
-
-    namespace ConsoleApplication1
-    {
-        class {|#0:TypeName|}
-        {   
-        }
-    }";
-
-            var fixtest = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
-
-    namespace ConsoleApplication1
-    {
-        class TYPENAME
-        {   
-        }
-    }";
-
-            var expected = VerifyCS.Diagnostic("DiscordInteractions").WithLocation(0).WithArguments("TypeName");
-            await VerifyCS.VerifyCodeFixAsync(test, expected, fixtest);
-        }
     }
 }
