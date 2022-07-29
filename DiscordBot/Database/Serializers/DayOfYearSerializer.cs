68928afa314f21423f09614606679934484a9c3e
TehGM <TehGM@users.noreply.github.com>
2022-07-29 17:54:06 +0100

    Added day of year serializer

---
+++ DiscordBot/Database/Serializers/DayOfYearSerializer.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+
+namespace DevSubmarine.DiscordBot.Database.Serializers
+{
+    public class DayOfYearSerializer : SerializerBase<DateTime>
+    {
+        public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, DateTime value)
+        {
+            context.Writer.WriteInt32(value.DayOfYear);
+        }
+
+        public override DateTime Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        {
+            int dayOfYear = context.Reader.ReadInt32();
+            DateTime result = new DateTime(DateTime.UtcNow.Year, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+                .AddDays(dayOfYear).Date;
+            return UpdatePastDate(result);
+        }
+
+        // we don't care about past birthdays, so add one year for those
+        public static DateTime UpdatePastDate(DateTime date)
+        {
+            while (date.Date < DateTime.UtcNow.Date)
+                date = date.AddYears(1).Date;
+            return date;
+        }
+    }
+}
