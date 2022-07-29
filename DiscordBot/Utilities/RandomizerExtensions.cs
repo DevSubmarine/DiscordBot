ba09c1b3be957211b02f72c2692eb50b6f72e699
TehGM <TehGM@users.noreply.github.com>
2022-07-29 21:09:50 +0100

    Add color randomizer extensions

---
+++ DiscordBot/Utilities/RandomizerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Discord;
+using TehGM.Utilities.Randomization;
+
+namespace DevSubmarine.DiscordBot
+{
+    public static class RandomizerExtensions
+    {
+        public static Color GetRandomDiscordColor(this IRandomizer randomizer)
+        {
+            int r = randomizer.GetRandomNumber(0, 255, true);
+            int g = randomizer.GetRandomNumber(0, 255, true);
+            int b = randomizer.GetRandomNumber(0, 255, true);
+            return new Color(r, g, b);
+        }
+    }
+}
