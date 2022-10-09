77f96fdae49ceb969e00f64296104ae37b2ded04
TehGM <TehGM@users.noreply.github.com>
2022-10-09 13:03:10 +0100

    Configurable autopost days ahead of bdays (1 day default)

--- DiscordBot/Features/Birthdays/IUserBirthdayEmbedBuilder.cs
+++ DiscordBot/Features/Birthdays/IUserBirthdayEmbedBuilder.cs
@@ -5,6 +5,6 @@ namespace DevSubmarine.DiscordBot.Birthdays
     public interface IUserBirthdayEmbedBuilder
     {
         Task<Embed> BuildUserBirthdayEmbedAsync(UserBirthday birthday, ulong? guildID, CancellationToken cancellationToken = default);
-        Task<Embed> BuildUpcomingBirthdaysEmbedAsync(IEnumerable<UserBirthday> birthdays, bool useEmotes, CancellationToken cancellationToken = default);
+        Task<Embed> BuildUpcomingBirthdaysEmbedAsync(IEnumerable<UserBirthday> birthdays, int daysAhead, bool useEmotes, CancellationToken cancellationToken = default);
     }
 }
