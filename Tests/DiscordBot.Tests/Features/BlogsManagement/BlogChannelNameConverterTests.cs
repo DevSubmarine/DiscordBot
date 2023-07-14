77cd5cef582e49e0b4d89400f60646705c5599b4
TehGM <TehGM@users.noreply.github.com>
2023-07-14 16:30:00 +0100

    Update blog name converter to new discord usernames

--- Tests/DiscordBot.Tests/Features/BlogsManagement/BlogChannelNameConverterTests.cs
+++ Tests/DiscordBot.Tests/Features/BlogsManagement/BlogChannelNameConverterTests.cs
@@ -8,8 +8,8 @@ namespace DevSubmarine.DiscordBot.Tests.Features.BlogsManagement
     {
         [Test]
         [TestCase("username")]
-        [TestCase("two words")]
-        [TestCase("with-dash")]
+        [TestCase("with.dot")]
+        [TestCase("with_underscore")]
         [TestCase("number123")]
         [Category(nameof(BlogChannelNameConverter.IsUsernameAllowed))]
         public void ValidUsername_IsAllowed(string username)
@@ -23,8 +23,8 @@ namespace DevSubmarine.DiscordBot.Tests.Features.BlogsManagement
 
         [Test]
         [TestCase("username", "blog-username")]
-        [TestCase("two words", "blog-two-words")]
-        [TestCase("with-dash", "blog-with-dash")]
+        [TestCase("with.dot", "blog-with-dot")]
+        [TestCase("with_underscore", "blog-with-underscore")]
         [TestCase("number123", "blog-number123")]
         [Category(nameof(BlogChannelNameConverter.ConvertUsername))]
         public void ValidUsername_IsConverted(string username, string expectedResult)
@@ -38,36 +38,11 @@ namespace DevSubmarine.DiscordBot.Tests.Features.BlogsManagement
 
         [Test]
         [TestCase("username", "blog-username")]
-        [TestCase("two words", "blog-two-words")]
-        [TestCase("with-dash", "blog-with-dash")]
+        [TestCase("with.dot", "blog-with-dot")]
+        [TestCase("with_underscore", "blog-with-underscore")]
         [TestCase("number123", "blog-number123")]
         [Category(nameof(BlogChannelNameConverterExtensions.TryConvertUsername))]
         public void ValidUsername_IsAllowedAndConverts(string username, string expectedResult)
-        {
-            BlogChannelNameConverter service = base.Fixture.Create<BlogChannelNameConverter>();
-
-            bool success = service.TryConvertUsername(username, out string result);
-
-            success.Should().BeTrue();
-            result.Should().Be(expectedResult);
-        }
-
-        [Test]
-        [TestCase("user'name", "blog-username")]
-        [Category(nameof(BlogChannelNameConverter.ConvertUsername))]
-        public void ValidUsername_WithAposthrope_ShouldRemoveApostrophe(string username, string expectedResult)
-        {
-            BlogChannelNameConverter service = base.Fixture.Create<BlogChannelNameConverter>();
-
-            string result = service.ConvertUsername(username);
-
-            result.Should().Be(expectedResult);
-        }
-
-        [Test]
-        [TestCase("user'name", "blog-username")]
-        [Category(nameof(BlogChannelNameConverterExtensions.TryConvertUsername))]
-        public void ValidUsername_WithAposthrope_ShouldSucceedAndRemoveApostrophe(string username, string expectedResult)
         {
             BlogChannelNameConverter service = base.Fixture.Create<BlogChannelNameConverter>();
 
@@ -105,12 +80,12 @@ namespace DevSubmarine.DiscordBot.Tests.Features.BlogsManagement
         }
 
         [Test]
-        [TestCase("trailingdash-", "blog-trailingdash")]
-        [TestCase("trailingspace ", "blog-trailingspace")]
-        [TestCase("-leadingdash", "blog-leadingdash")]
-        [TestCase(" leadingspace", "blog-leadingspace")]
+        [TestCase("trailingdot.", "blog-trailingdot")]
+        [TestCase("trailingunderscore_", "blog-trailingunderscore")]
+        [TestCase(".leadingdot", "blog-leadingdot")]
+        [TestCase("_leadingunderscore", "blog-leadingunderscore")]
         [Category(nameof(BlogChannelNameConverter.ConvertUsername))]
-        public void ValidUsername_WithSpacesOrDashes_ShouldTrim(string username, string expectedResult)
+        public void ValidUsername_WithSpacesUnderscoresOrDashes_ShouldTrim(string username, string expectedResult)
         {
             BlogChannelNameConverter service = base.Fixture.Create<BlogChannelNameConverter>();
 
@@ -120,12 +95,12 @@ namespace DevSubmarine.DiscordBot.Tests.Features.BlogsManagement
         }
 
         [Test]
-        [TestCase("trailingdash-", "blog-trailingdash")]
-        [TestCase("trailingspace ", "blog-trailingspace")]
-        [TestCase("-leadingdash", "blog-leadingdash")]
-        [TestCase(" leadingspace", "blog-leadingspace")]
+        [TestCase("trailingdot.", "blog-trailingdot")]
+        [TestCase("trailingunderscore_", "blog-trailingunderscore")]
+        [TestCase(".leadingdot", "blog-leadingdot")]
+        [TestCase("_leadingunderscore", "blog-leadingunderscore")]
         [Category(nameof(BlogChannelNameConverterExtensions.TryConvertUsername))]
-        public void ValidUsername_WithSpacesOrDashes_ShouldSucceedAndTrim(string username, string expectedResult)
+        public void ValidUsername_WithSpacesUnderscoresOrDashes_ShouldSucceedAndTrim(string username, string expectedResult)
         {
             BlogChannelNameConverter service = base.Fixture.Create<BlogChannelNameConverter>();
 
@@ -136,83 +111,8 @@ namespace DevSubmarine.DiscordBot.Tests.Features.BlogsManagement
         }
 
         [Test]
-        [TestCase("a")]
-        [TestCase("ab")]
-        [Category(nameof(BlogChannelNameConverter.IsUsernameAllowed))]
-        public void InvalidUsername_TooShort_IsNotAllowed(string username)
-        {
-            BlogChannelNameConverter service = base.Fixture.Create<BlogChannelNameConverter>();
-
-            bool result = service.IsUsernameAllowed(username);
-
-            result.Should().BeFalse();
-        }
-
-        [Test]
-        [TestCase("a")]
-        [TestCase("ab")]
-        [Category(nameof(BlogChannelNameConverter.ConvertUsername))]
-        public void InvalidUsername_TooShort_Throws(string username)
-        {
-            BlogChannelNameConverter service = base.Fixture.Create<BlogChannelNameConverter>();
-
-            Action act = () => service.ConvertUsername(username);
-
-            act.Should().Throw<ArgumentException>();
-        }
-
-        [Test]
-        [TestCase("a")]
-        [TestCase("ab")]
-        [Category(nameof(BlogChannelNameConverterExtensions.TryConvertUsername))]
-        public void InvalidUsername_TooShort_ShouldFail(string username)
-        {
-            BlogChannelNameConverter service = base.Fixture.Create<BlogChannelNameConverter>();
-
-            bool success = service.TryConvertUsername(username, out string result);
-
-            success.Should().BeFalse();
-        }
-
-        [Test]
-        [TestCase("qwertyuiopasdfghjk")]
-        [Category(nameof(BlogChannelNameConverter.IsUsernameAllowed))]
-        public void InvalidUsername_TooLong_IsNotAllowed(string username)
-        {
-            BlogChannelNameConverter service = base.Fixture.Create<BlogChannelNameConverter>();
-
-            bool result = service.IsUsernameAllowed(username);
-
-            result.Should().BeFalse();
-        }
-
-        [Test]
-        [TestCase("qwertyuiopasdfghjk")]
-        [Category(nameof(BlogChannelNameConverter.ConvertUsername))]
-        public void InvalidUsername_TooLong_Throws(string username)
-        {
-            BlogChannelNameConverter service = base.Fixture.Create<BlogChannelNameConverter>();
-
-            Action act = () => service.ConvertUsername(username);
-
-            act.Should().Throw<ArgumentException>();
-        }
-
-        [Test]
-        [TestCase("qwertyuiopasdfghjk")]
-        [Category(nameof(BlogChannelNameConverterExtensions.TryConvertUsername))]
-        public void InvalidUsername_TooLong_ShouldFail(string username)
-        {
-            BlogChannelNameConverter service = base.Fixture.Create<BlogChannelNameConverter>();
-
-            bool success = service.TryConvertUsername(username, out string result);
-
-            success.Should().BeFalse();
-        }
-
-        [Test]
-        [TestCase("blog-something")]
-        [TestCase("chat-something")]
+        [TestCase("blog_something")]
+        [TestCase("chat_something")]
         [Category(nameof(BlogChannelNameConverter.IsUsernameAllowed))]
         public void InvalidUsername_StartsWithForbiddenWord_IsNotAllowed(string username)
         {
@@ -224,8 +124,8 @@ namespace DevSubmarine.DiscordBot.Tests.Features.BlogsManagement
         }
 
         [Test]
-        [TestCase("blog-something")]
-        [TestCase("chat-something")]
+        [TestCase("blog_something")]
+        [TestCase("chat_something")]
         [Category(nameof(BlogChannelNameConverter.ConvertUsername))]
         public void InvalidUsername_StartsWithForbiddenWord_Throws(string username)
         {
@@ -237,8 +137,8 @@ namespace DevSubmarine.DiscordBot.Tests.Features.BlogsManagement
         }
 
         [Test]
-        [TestCase("blog-something")]
-        [TestCase("chat-something")]
+        [TestCase("blog_something")]
+        [TestCase("chat_something")]
         [Category(nameof(BlogChannelNameConverterExtensions.TryConvertUsername))]
         public void InvalidUsername_StartsWithForbiddenWord_ShouldFail(string username)
         {
@@ -253,8 +153,8 @@ namespace DevSubmarine.DiscordBot.Tests.Features.BlogsManagement
         [TestCase("foobar")]
         [TestCase("abcfoobarabc")]
         [TestCase("abcfOOBarabc")]
-        [TestCase("abc-foobar-abc")]
-        [TestCase("abc-fOOBar-abc")]
+        [TestCase("abc_foobar_abc")]
+        [TestCase("abc_fOOBar_abc")]
         [Category(nameof(BlogChannelNameConverter.IsUsernameAllowed))]
         public void InvalidUsername_WithForbiddenWord_IsNotAllowed(string username)
         {
@@ -271,8 +171,8 @@ namespace DevSubmarine.DiscordBot.Tests.Features.BlogsManagement
         [TestCase("foobar")]
         [TestCase("abcfoobarabc")]
         [TestCase("abcfOOBarabc")]
-        [TestCase("abc-foobar-abc")]
-        [TestCase("abc-fOOBar-abc")]
+        [TestCase("abc_foobar_abc")]
+        [TestCase("abc_fOOBar_abc")]
         [Category(nameof(BlogChannelNameConverter.ConvertUsername))]
         public void InvalidUsername_WithForbiddenWord_Throws(string username)
         {
@@ -289,8 +189,8 @@ namespace DevSubmarine.DiscordBot.Tests.Features.BlogsManagement
         [TestCase("foobar")]
         [TestCase("abcfoobarabc")]
         [TestCase("abcfOOBarabc")]
-        [TestCase("abc-foobar-abc")]
-        [TestCase("abc-fOOBar-abc")]
+        [TestCase("abc_foobar_abc")]
+        [TestCase("abc_fOOBar_abc")]
         [Category(nameof(BlogChannelNameConverterExtensions.TryConvertUsername))]
         public void InvalidUsername_WithForbiddenWord_ShouldFail(string username)
         {
